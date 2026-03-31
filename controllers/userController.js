982ad24088c9e515b8b7c37b6cfa1f2f72e55c9e
evilasiojunio63-netizen <evilasiojunio63@gmail.com>
2026-03-31 16:00:59 -0300

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,33 @@
+import User from "../models/User.js";
 
+// Criar usuário
+export const registerUser = async (req, res) => {
+  const user = await User.create(req.body);
+  res.json(user);
+};
+
+// Dar like
+export const likeUser = async (req, res) => {
+  const { userId, targetId } = req.body;
+
+  const user = await User.findById(userId);
+  const target = await User.findById(targetId);
+
+  if (!user.likes.includes(targetId)) {
+    user.likes.push(targetId);
+    await user.save();
+  }
+
+  // verificar match
+  if (target.likes.includes(userId)) {
+    user.matches.push(targetId);
+    target.matches.push(userId);
+
+    await user.save();
+    await target.save();
+
+    return res.json({ match: true });
+  }
+
+  res.json({ match: false });
+};

2f35ecb2564127d5a626aebe347b8aedbee5f0f5
evilasiojunio63-netizen <evilasiojunio63@gmail.com>
2026-03-31 16:02:14 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
 
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.use("/api/users", userRoutes);
+
+app.listen(5000, () => {
+  console.log("Servidor rodando na porta 5000");
+});

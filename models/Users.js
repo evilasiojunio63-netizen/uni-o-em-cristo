dff5ccae70a0de50045fae723d46c66451924547
evilasiojunio63-netizen <evilasiojunio63@gmail.com>
2026-03-31 16:00:31 -0300

    Update Users.js

--- models/Users.js
+++ models/Users.js
@@ -1,1 +1,12 @@
+import mongoose from "mongoose";
 
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  bio: String,
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  matches: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+});
+
+export default mongoose.model("User", userSchema);

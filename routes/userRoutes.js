2435cd3a19f985d8f4f55527dea499e01bc4d1be
evilasiojunio63-netizen <evilasiojunio63@gmail.com>
2026-03-31 16:01:33 -0300

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,1 +1,9 @@
+import express from "express";
+import { registerUser, likeUser } from "../controllers/userController.js";
 
+const router = express.Router();
+
+router.post("/register", registerUser);
+router.post("/like", likeUser);
+
+export default router;
